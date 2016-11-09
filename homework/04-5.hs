7eae6a7a125c2ef141500accebc60f94b7182b61
toni <tkukurin@gmail.com>
2016-11-09 18:10:09 +0100

    fixed tabbing

--- homework/04-5.hs
+++ homework/04-5.hs
@@ -5,14 +5,14 @@ type Tolerance = Double
 
 newton :: Tolerance -> Double -> Double
 newton tolerance value = 
-						if value < 0 || tolerance < 0
-								then error "Both value and tolerance must be positive"
-								else newton' tolerance value value 
-						where newton' t x y =
-											if (abs (newtonNext - y)) < t
-													then newtonNext
-													else newton' t x newtonNext
-											where newtonNext = (y + (x/y)) / 2
+	if value < 0 || tolerance < 0
+			then error "Both value and tolerance must be positive"
+			else newton' tolerance value value 
+	where newton' t x y =
+						if (abs (newtonNext - y)) < t
+								then newtonNext
+								else newton' t x newtonNext
+						where newtonNext = (y + (x/y)) / 2
 
 -- derivative
 deriv :: (Double -> Double) -> Double -> Double

931649d7c770c4bc3210ba9f2ae564cc3d3da9ba
toni <tkukurin@gmail.com>
2016-12-20 22:43:19 +0100

    05-4 (tracks)

--- homework/05-4.hs
+++ homework/05-4.hs
@@ -3,9 +3,12 @@ import Data.List
 allNums :: String -> Bool
 allNums xs = (length xs) == (length $ filter (\x -> x `elem` "0123456789") xs)
 
-getTrackNo :: [String] -> Int
-getTrackNo xs = (read $ head $ filter allNums xs) :: Int
+getFirstNumber :: [String] -> Integer
+getFirstNumber = read . head . filter allNums
 
 sortTracks :: [String] -> [String]
-sortTracks = map unwords . sortOn getTrackNo . map words
+sortTracks = map unwords . sortOn getFirstNumber . map words
+
+numberOfPlays :: [String] -> Integer
+numberOfPlays = sum . map getFirstNumber . map words
 

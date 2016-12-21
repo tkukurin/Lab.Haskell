d02f35e199450f5984cb671fac982e65757a1058
toni <tkukurin@gmail.com>
2016-12-21 02:00:59 +0100

    05-3 removed commented part

--- homework/05-3.hs
+++ homework/05-3.hs
@@ -1,9 +1,6 @@
 
 data Pred = Val Bool | Or Pred Pred | And Pred Pred | Not Pred deriving (Show)
 
-{-Or (Val first) (Val second) = Val (first || second)
-And (Val first1) (Val second1) = Val (first1 && second1)-}
-
 eval :: Pred -> Bool
 eval (Val x)     = x
 eval (Not x)     = not (eval x)
